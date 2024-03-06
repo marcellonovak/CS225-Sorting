6d8c010f81c6ec2c303152012bf61f86ccb4a784
fclerget <114555886+fclerget@users.noreply.github.com>
2024-03-05 22:24:42 -0700

    started bubble

--- SortBubble.h
+++ SortBubble.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <numeric>
+#include <algorithm> // include for swap
+using namespace std;
+
+int* bubbleSort(int arr[], int n) {
+	bool swaps = false;
+
+	for (int i = 0; i < n - 1; i++) {
+		swaps = false;
+		for (int j = 0; j < n - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				swaps = true;
+			}
+		}
+		if (swaps == true) {
+			break;
+		}
+	}
+
+	return arr;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "SortSelection.h"
+using namespace std;
+
+int main() {
+	int arr[7] = { 7, 3, 20, 4, 4, 0, 9 };
+	int n = sizeof(arr);
+
+	//selectionSort(arr, n);
+	return 0;
+}
